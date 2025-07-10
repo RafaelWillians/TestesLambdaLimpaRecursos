59f30d0918d4ceab0027f37183c32938db5b0cd7
Rafael Silva Willians <rafael.willians@outlook.com>
2025-07-10 17:10:32 +0000

    Adicionado codigo inicial

--- python/limpa_eventbridge.py
+++ python/limpa_eventbridge.py
@@ -1,3 +1,10 @@
 import boto3
 import os
 import time
+
+def cleanup_eventbridge(, region):
+    print(f"[{region}] --- Iniciando limpeza do EventBridge")
+
+    # Excluir Regras do EventBridge
+    try:
+        
